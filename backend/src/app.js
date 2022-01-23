5bcf49312060c4a5c400b5b661fe502ab4550765
leonardoosouza1 <leonardo.vilson@hotmail.com>
2022-01-22 23:38:19 -0300

    feat: created app

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import index from './routes/index.js'
+const app = express()
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+app.use(index)
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port: ', process.env.PORT || 3000)
+})
+export default app
